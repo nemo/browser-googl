64b2b41447a11216261ecbe4e8382d2f51caa42e
Nima Gardideh <nima@halfmoon.ws>
2015-11-06 15:09:30 -0800

    Added ability to set API key.

--- index.js
+++ index.js
@@ -5,25 +5,36 @@ var request = require("browser-request");
  * @type {Object}
  */
 module.exports = {
+  setKey: function(key) {
+      exports.API_KEY = key;
+  },
   short: function(url, onComplete) {
+    var url = 'https://www.googleapis.com/urlshortener/v1/url';
+    if (exports.API_KEY)
+      url += '?key=' + exports.API_KEY;
+
     request(
-      { 
-        method:'POST', 
-        url:'https://www.googleapis.com/urlshortener/v1/url', 
-        body:'{\"longUrl\": \"' + url + '\" }', 
+      {
+        method:'POST',
+        url:url,
+        body:'{\"longUrl\": \"' + url + '\" }',
         json:true
-      }, 
+      },
       function(err, response, body) {
         onComplete(err, body);
       });
   },
   expand: function(url, onComplete) {
+      var fullUrl = 'https://www.googleapis.com/urlshortener/v1/url?shortUrl=' + url;
+      if (exports.API_KEY)
+        fullUrl += '&key=' + exports.API_KEY;
+
     request(
-      { 
+      {
         method:'GET', 
-        url:'https://www.googleapis.com/urlshortener/v1/url?shortUrl=' + url,
+        url:fullUrl,
         json:true
-      }, 
+      },
       function(err, response, body) {
         onComplete(err, body);
       });
